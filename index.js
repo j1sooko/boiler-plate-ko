44f0c7d5ef5e0575d49e06bc6e291b0f2c1a6bbb
jisoo ko <doubletry23@gmail.com>
2023-08-15 21:44:41 +0900

    feat: 비밀 정보 보호

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express() // 새로운 express 앱 생성
 const port = 5000 // 3000 4000 5000 모두 ok
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
+const config = require('./config/key');
 
 // application/x-www-form-urlencoded 로 된 데이터를 분석해서 가져올 수 있게 해줌
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://doubletry23:GuhvnJMyF3CouEAp@boilerplate.zhapsin.mongodb.net/', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true // 에러 방지
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -19,7 +20,7 @@ mongoose.connect('mongodb+srv://doubletry23:GuhvnJMyF3CouEAp@boilerplate.zhapsin
 
 
 
-app.get('/', (req, res) => res.send('Hello World! 우와~')) // 루트 dir에 hello world 출력
+app.get('/', (req, res) => res.send('Hello World! 우와~ 222')) // 루트 dir에 hello world 출력
 
 
 // register route
